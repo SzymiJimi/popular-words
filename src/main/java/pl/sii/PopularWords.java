0c86570248803b80733ebfa27c8902e807c59548
SzymiJimi <rekas1@tlen.pl>
2019-05-14 23:49:43 +0200

    Finished task

--- src/main/java/pl/sii/PopularWords.java
+++ src/main/java/pl/sii/PopularWords.java
@@ -1,10 +1,43 @@
 package pl.sii;
 
-import org.apache.commons.lang3.NotImplementedException;
-
-import java.util.Map;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PopularWords {
+
+
+    private String readFileToString() throws IOException {
+        return new String(Files.readAllBytes(Paths.get("src\\main\\resources\\3esl.txt")));
+    }
+
+    private ArrayList<String> splitWordsIntoArray(String content){
+        return new ArrayList<>(Arrays.asList( content.split("[-]+|[ ]+|(?=')+|[\n]+|[\r]+")));
+    }
+
+    private HashMap<String, Long> countOccurrences(ArrayList<String> words){
+        HashMap<String, Long> wordsCollection = new HashMap<>();
+        words.stream()
+                .filter(word -> word.matches("([^ ])([^ ]*)([^\\n]*)([^']+)"))
+                .forEach(word ->{
+                    Long value = wordsCollection.get(word.toLowerCase());
+                    if(value!=null){
+                        value++;
+                        wordsCollection.put(word.toLowerCase(), value);
+                    }else{
+                        wordsCollection.put(word.toLowerCase(), 1L);
+                    }
+                });
+
+        return wordsCollection.entrySet()
+                .stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .limit(1000)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2)->e1, LinkedHashMap::new));
+
+    }
 
     public static void main(String[] args) {
         PopularWords popularWords = new PopularWords();
@@ -13,6 +46,13 @@ public class PopularWords {
     }
 
     public Map<String, Long> findOneThousandMostPopularWords() {
-        throw new NotImplementedException("TODO implementation");
+        try{
+            String content = this.readFileToString();
+            return countOccurrences(splitWordsIntoArray(content));
+        }catch(Exception e){
+            System.out.println("Error with file loading: " + e.toString());
+            return new HashMap<>();
+        }
+
     }
 }
--- src/test/java/pl/sii/PopularWordsTest.java
+++ src/test/java/pl/sii/PopularWordsTest.java
@@ -3,8 +3,14 @@ package pl.sii;
 import org.apache.commons.lang3.NotImplementedException;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
@@ -35,7 +41,10 @@ public class PopularWordsTest {
 
         result.forEach((key, value) -> {
             BigDecimal valueUsagePercentage = calculatePercentage(value, totalFrequencyInAResult);
-            BigDecimal kilgarriffUsagePercentage = calculatePercentage(wordsFrequencyListCreatedByAdamKilgarriff.get(key), totalFrequencyByKilgarriff);
+            Long frequency  = wordsFrequencyListCreatedByAdamKilgarriff.get(key);
+            if(frequency==null)
+                frequency = 0L;
+            BigDecimal kilgarriffUsagePercentage = calculatePercentage(frequency, totalFrequencyByKilgarriff);
             BigDecimal diff = kilgarriffUsagePercentage.subtract(valueUsagePercentage);
             System.out.println(key + "," + valueUsagePercentage + "%," + kilgarriffUsagePercentage + "%," + (new BigDecimal(0.5).compareTo(diff.abs()) > 0) + " " + diff);
         });
@@ -45,7 +54,31 @@ public class PopularWordsTest {
         return BigDecimal.valueOf(obtained * 100 / total).setScale(4, RoundingMode.HALF_UP);
     }
 
+    private String readFileToString()  {
+        try {
+            return new String(Files.readAllBytes(Paths.get("src\\test\\resources\\all.num")));
+        } catch (IOException e) {
+            System.out.println("Error with file loading!" + e.toString());
+            return "";
+        }
+    }
+
+    private Map<String, Long> loadWordsIntoMap(String file){
+        if(file.equals(""))
+            return new HashMap<>();
+
+        ArrayList<String> lines =  new ArrayList<>(Arrays.asList( file.split("[\n]")));
+        HashMap<String, Long> wordsFrequency = new HashMap<>();
+        lines.stream()
+                .skip(1)
+                .forEach(line -> {
+                     var wordsInLine = line.split("[ ]");
+                     wordsFrequency.put(wordsInLine[1], Long.valueOf( wordsInLine[0]));
+                });
+        return wordsFrequency;
+    }
+
     private Map<String, Long> getWordsFrequencyListCreatedByAdamKilgarriff() {
-        throw new NotImplementedException("TODO implementation");
+        return loadWordsIntoMap(readFileToString());
     }
 }
